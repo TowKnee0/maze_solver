a883d5d8078d93342ab7a8a670cbe055a3052362
TowKnee0 <Tonyyqhe@gmail.com>
2021-03-09 21:28:02 -0500

    Implemented BFS and visualization

--- main.py
+++ main.py
@@ -4,13 +4,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 from typing import Tuple
 import numba
+import pygame
 
-image = cv2.imread('maze2.jpg')
+pygame.init()
+
+image = cv2.imread('maze.png')
 
 retVal, thresh = cv2.threshold(cv2.cvtColor(image, cv2.COLOR_RGB2GRAY), 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
 thinned = np.array(cv2.ximgproc.thinning(thresh))//255
 
+# cut borders
+
+temp = np.delete(thinned, [0, thinned.shape[1] - 1], axis=1)
+temp = np.delete(thinned, [0, thinned.shape[0] - 1], axis=0)
 
 # This along with the decorator complies each method into machine code the first
 # time its called. Then each subsequent call runs from machine code meaning
@@ -24,8 +31,8 @@ thinned = np.array(cv2.ximgproc.thinning(thresh))//255
 # Note: the first call to a method may be slower
 spec = [
         ('graph', numba.uint8[:, :]),
-        ('rows', numba.uint8),
-        ('cols', numba.uint8)
+        ('rows', numba.uint16),
+        ('cols', numba.uint16)
         ]
 
 
@@ -36,10 +43,7 @@ class MatrixGraph(object):
     8 nodes (pixels) around each node.
 
     Note: This is a jitclass so type of inputs is very sensitive.
-
-
     """
-
 
     graph: np.ndarray
     rows: int
@@ -47,11 +51,13 @@ class MatrixGraph(object):
 
     def __init__(self, matrix: np.ndarray) -> None:
         self.graph = matrix
-        self.rows, self.cols = matrix.shape
+        self.cols, self.rows = matrix.shape
 
     def get_valid_neighbours(self, row: int, col: int) -> list[Tuple]:
         """Not elegant but fast(enough for now)"""
         neighbours = []
+        # col = cord[0]
+        # row = cord[1]
 
         if row - 1 >= 0 and self.graph[row - 1, col] == 1:
             neighbours.append((row - 1, col))
@@ -74,18 +80,71 @@ class MatrixGraph(object):
 
 class PathfindingAlgorithms:
 
-    def depth_first_search(self, graph: MatrixGraph, curr: Tuple[int, int], target: Tuple[int, int], visited):
+    def depth_first_search(self, graph: MatrixGraph, curr: Tuple[int, int], target: Tuple[int, int], visited, surface):
         if curr == target:
             return True
+
+        surface.set_at(curr, (255, 0, 0))
+        display.blit(surf, (0, 0))
+        pygame.display.flip()
 
         visited.append(curr)
         for node in graph.get_valid_neighbours(curr[0], curr[1]):
             if node not in visited:
-                self.depth_first_search(graph, node, target, visited)
+                self.depth_first_search(graph, node, target, visited, surface)
+        return False
+
+    def breadth_first_search(self, graph, start, target, visited: set, surface):
+        queue = []
+        queue.extend(graph.get_valid_neighbours(start[0], start[1]))
+        visited.update(graph.get_valid_neighbours(start[0], start[1]))
+
+
+        print(queue)
+
+        while queue != []:
+
+            curr = queue.pop(0)
+
+            surface.set_at(curr, (255, 0, 0))
+            display.blit(surf, (0, 0))
+            pygame.display.flip()
+
+            if curr == target:
+                return True
+            for node in graph.get_valid_neighbours(curr[0], curr[1]):
+                if node not in visited:
+                    queue.append(node)
+                    visited.add(node)
         return False
 
 
-graph1 = MatrixGraph(thinned)
+
+
+graph1 = MatrixGraph(temp)
+
+pygame.init()
+display = pygame.display.set_mode((1000, 1000))
+
+surf = pygame.surfarray.make_surface(graph1.graph * 255)
+#
+display.blit(surf, (0, 0))
+alg = PathfindingAlgorithms()
+# alg.depth_first_search(graph1, (719, 859), (1, 432), [], surf) //this needs to be implemented iteratively
+alg.breadth_first_search(graph1, (719, 859), (393, 432), set(), surf)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        if event.type == pygame.MOUSEMOTION:
+            temp = pygame.mouse.get_pos()
+            try:
+                print(temp, graph1.graph[temp])
+            except:
+                pass
+
+    pygame.display.flip()
 
 # cv2.imshow('w', thinned)
 #
