95d0a488d63ce0dfa44eb43c7d1a9fa817e4db2f
Austin <39536513+AustinG1T@users.noreply.github.com>
2021-04-10 15:03:21 -0400

    Added the final path

--- algorithms.py
+++ algorithms.py
@@ -19,17 +19,23 @@ class PathfindingAlgorithms:
     """
 
     def breadth_first_search(self, graph: MatrixGraph, start: tuple, target: tuple, visited: set,
-                             surface, display) -> bool:
+                             surface, display) -> list[tuple]:
         """
         Return true if target is found, return false otherwise.
 
         This function is an implementation of the breadth_first_search pathfinding algorithm
         """
         queue = []
+        paths = {}
         queue.extend(graph.get_valid_neighbours(start[0], start[1]))
         visited.update(graph.get_valid_neighbours(start[0], start[1]))
 
-        while queue != []:
+        # Update paths with the original visited set
+        for node in graph.get_valid_neighbours(start[0], start[1]):
+            paths[node] = start
+        found = False
+
+        while queue != [] and not found:
 
             curr = queue.pop(0)
 
@@ -39,15 +45,40 @@ class PathfindingAlgorithms:
             pygame.display.flip()
 
             if curr == target:
-                return True
+                found = True
             for node in graph.get_valid_neighbours(curr[0], curr[1]):
                 if node not in visited:
                     queue.append(node)
                     visited.add(node)
-        return False
+
+                    # Add the node as a key with the current node as the value
+                    paths[node] = curr
+
+
+        if found is False:
+            return []
+        else:
+            # Trace the path back to the start, the list is traversed backwards, so we insert to
+            # the front of the list
+            final_path = []
+            current_node = target
+            while current_node != start:
+                final_path.insert(0, current_node)
+                current_node = paths[current_node]
+                # Draw the final path
+                pygame.draw.circle(surface, (0, 255, 0), current_node, 3)
+
+            # Insert the first node
+            final_path.insert(0, start)
+
+            # Draw the final path
+            pygame.draw.circle(surface, (0, 255, 0), start, 3)
+            display.blit(surface, (0, 0))
+            pygame.display.flip()
+            return final_path
 
     def depth_first_search_iterative(self, graph: MatrixGraph, start: tuple, target: tuple,
-                                     surface, display) -> bool:
+                                     surface, display) -> list[tuple[int, int]]:
         """
         Return true if target is found, return false otherwise.
 
@@ -57,8 +88,10 @@ class PathfindingAlgorithms:
         discovered = set()
         stack = [start]  # Stack is a reversed list for now. Later we can make a stack class if we
         # need
+        paths = {}  # A dictionary that maps new nodes to the previous node
+        found = False
 
-        while stack != []:
+        while stack != [] and not found:
             vertex = stack.pop()
 
             # Visualize step
@@ -67,10 +100,35 @@ class PathfindingAlgorithms:
             pygame.display.flip()
 
             if vertex == target:
-                return True
+                found = True
             elif vertex not in discovered:
                 discovered.add(vertex)
                 neighbors = graph.get_valid_neighbours(vertex[0], vertex[1])
                 for neighbor in neighbors:
                     stack.append(neighbor)
-        return False
+                    if neighbor not in discovered:
+                        # Add the neighbor as a key in the path dictionary with vertex as a parent
+                        paths[neighbor] = vertex
+
+        if found is False:
+            return []
+        else:
+            # Trace the path back to the start, the list is traversed backwards, so we insert to
+            # the front of the list
+            final_path = []
+            current_node = target
+            while current_node != start:
+                final_path.insert(0, current_node)
+                current_node = paths[current_node]
+                # Draw the final path
+                pygame.draw.circle(surface, (0, 255, 0), current_node, 3)
+
+            # Insert the first node
+            final_path.insert(0, start)
+
+            # Draw the final path
+            pygame.draw.circle(surface, (0, 255, 0), start, 3)
+            display.blit(surface, (0, 0))
+            pygame.display.flip()
+            return final_path
+
--- main.py
+++ main.py
@@ -68,7 +68,7 @@ while True:
 
     # print(start, end)
     if start is not None and end is not None and once:
-        alg.breadth_first_search(graph1, start, end, set(), surf, display)
+        print(alg.breadth_first_search(graph1, start, end, set(), surf, display))
         once = False
 
     # Draw the buttons
